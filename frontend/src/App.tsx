1f14a5b5caba6498e67ee69fea399b25fe7b758e
Alex <polonnikov.alexander@gmail.com>
2023-08-28 17:39:28 +0300

    commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,13 +6,23 @@ import NaviBar from './Components/NaviBar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import AuthForm from './Components/auth/AuthForm';
 import { QueryClient } from 'react-query';
+import { useQuery } from 'react-query';
+import axios from 'axios';
 
 
 const queryClient = new QueryClient();
 
+
+async function fetchData(){
+  const response = await 
+}
+
 function App() {
   return (
     <div className="App">
+      
+
+
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
