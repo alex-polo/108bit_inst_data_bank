943443fcf7b0ff392e48c19922eba00803d7a143
Alex <polonnikov.alexander@gmail.com>
2023-08-29 06:37:06 +0300

    commit

--- server/main.py
+++ server/main.py
@@ -1,8 +1,10 @@
 import logging
+import time
 
 import uvicorn
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
+from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import JSONResponse
 
 import database
@@ -11,6 +13,18 @@ from server.etc import DatabaseConfig, get_database_config
 logger = logging.getLogger(__name__)
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=["*"],
+)
 
 
 @app.on_event("startup")
@@ -25,7 +39,8 @@ def registry_routers() -> None:
 @app.get('/')
 def home():
     try:
-        return JSONResponse(status_code=200, content={'app': 'Data Bank', 'version': 0.01, 'data': None})
+        time.sleep(1)
+        return JSONResponse(status_code=200, content={'app': 'Data Bank', 'version': '0.01a', 'data': 'any data'})
     except Exception as error:
         raise HTTPException(status_code=500, detail={
                 'app': 'Data Bank',
